bbb795ecd186cfe1c66ebe3772a17cc8f785d572
Julia Pavlova <jul11112@yandex.ru>
2022-07-11 17:15:41 +0300

    Решение 4 задачи

---
+++ task7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе 
+// показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Write a number: ");
+int n = int.Parse(Console.ReadLine());
+
+int i = 2;
+
+while (i <= n)
+{
+    Console.WriteLine(i);
+    i+=2;
+}
+
