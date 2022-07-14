bc1fb3bb7e74bbec9a814f3a85002b87063d9323
Julia Pavlova <jul11112@yandex.ru>
2022-07-14 15:36:06 +0300

    2_2

---
+++ 2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и 
+// удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int a = new Random().Next(100,999);
+Console.WriteLine(a);
+
+int a1digit  = a / 100;
+int a3digit = a %10;
+Console.WriteLine(a1digit %10);
+Console.WriteLine(a3digit);
+
+Console.WriteLine($"{a1digit}{a3digit}");
