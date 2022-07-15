049ab95eaed00d40dc186c94df573f0bfac75491
Julia Pavlova <jul11112@yandex.ru>
2022-07-15 23:58:55 +0300

    2_1

---
+++ 2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+ 
+Console.WriteLine("Write a three-digit number: ");
+int n = int.Parse(Console.ReadLine());
+
+int n2digit  = n / 10;
+
+Console.WriteLine(n2digit %10);
