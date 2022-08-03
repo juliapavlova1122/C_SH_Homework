c5df0f9511ed7c4b6de34fb6538f076bce16ed83
Julia Pavlova <jul11112@yandex.ru>
2022-08-03 19:00:58 +0300

    4_1

---
+++ 4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int Nums (int a, int b)
+
+{
+    int exponentiation = a;
+    for(int i = 1; i<b; i++)
+    exponentiation = exponentiation * a;
+    return exponentiation;
+}
+
+Console.WriteLine(Nums(3, 5));
+
