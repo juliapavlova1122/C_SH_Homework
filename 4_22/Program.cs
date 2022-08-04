afc1d68f439b8c2afd2916bc412f283d804bfee2
Julia Pavlova <jul11112@yandex.ru>
2022-08-04 22:23:41 +0300

    4_22

---
+++ 4_22/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// Решение через string
+
+
+double DigitSum(int num)
+{
+string n = num.ToString();
+int length = n.Length;
+double sum = 0;
+for (int i = 0; i < length; i++)
+{
+
+double m = char.GetNumericValue(n[i]);
+
+sum += m;
+}
+
+return sum;
+}
+
+int n = 452;
+
+Console.WriteLine(DigitSum(n));
