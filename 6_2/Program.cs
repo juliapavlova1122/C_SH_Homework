f2d9d3236fbf6a258b5923aac30bf3d7e26e15ad
Julia Pavlova <jul11112@yandex.ru>
2022-08-10 15:17:50 +0300

    6_2

--- 6_2/Program.cs
+++ 6_2/Program.cs
@@ -1,67 +1,29 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.WriteLine("Введите {m} чисел: ");
-int n = int.Parse(Console.ReadLine());
+int m = 5;
+Console.Write($"Введите {m} целых чисел через пробел: ");
+string new_str = Console.ReadLine();
 
-Console.WriteLine("Введите следующее число: ");
-int n_1 = int.Parse(Console.ReadLine());
-
-int i = 1;
-while (i < m)
-{
-    Console.Write(i);
-    i++;
+while (new_str.Split().Length != m)
+{   
+    Console.WriteLine($"Введено неверное количество чисел - {new_str.Split().Length} ");
+    Console.WriteLine($"Пожалуйста, обратите внимание, что необходимо ввести {m} чисел: ");
+    Console.Write($"Введите {m} целых чисел через пробел: ");
+    new_str = Console.ReadLine();
 }
 
-// Задача 27: Напишите программу, которая принимает на вход число и 
-// выдаёт сумму цифр в числе.
-// 452 -> 11
+int[] arr = Array.ConvertAll(new_str.Split(), int.Parse);
+int count = 0;
 
-
-double DigitSum(int num)
+for (int i = 0; i < arr.Length; i++)
 {
-string n = num.ToString();
-int length = n.Length;
-double sum = 0;
-for (int i = 0; i < length; i++)
-{
-
-double m = char.GetNumericValue(n[i]);
-
-sum += m;
-}
-
-return sum;
-}
-
-int n = 452;
-
-
-
-void Print(double[] arr)
-{
-    int size = arr.Length;
-
-    for (int i = 0; i < size; i++)
+    if (arr[i] > 0)
     {
-        Console.Write($"{arr[i]} ");
+        count++;
     }
-    Console.WriteLine();
 }
+Console.WriteLine($"Из них {count} больше 0 ");
 
-double[] MassNums(int size)
-{
-    double[] arr = new double[size];
-    
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = Math.Round (new Random().NextDouble() * (10-1) + 1, 2);
-            }
-    return arr;
-}
-
-double[] arr_1 = MassNums(10);
-Print(arr_1);
