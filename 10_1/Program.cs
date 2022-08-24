c1d41dc79c9487bf3dc3bca32d7614d8d20c59a7
Julia Pavlova <jul11112@yandex.ru>
2022-08-24 12:33:27 +0300

    10_1

---
+++ 10_1/Program.cs
@@ -0,0 +1,24 @@
+﻿//  2. Даны два числа. 
+// Показать большее и меньшее число
+
+// 11. Дано число из отрезка [10, 99]. 
+// Показать наибольшую цифру числа
+
+// Console.WriteLine("Введите число от 10 до 99: ");
+// int n = int.Parse(Console.ReadLine());
+
+void Numbers (int a)
+{
+    if (a < 10 || a > 99)
+    Console.WriteLine ("Введите число от 10 до 99");
+    else
+    {
+        if (a % 10 > a / 10)
+        Console.WriteLine($"{a % 10}");
+        else Console.WriteLine ($"{a/10}");
+    } 
+}
+
+Console.WriteLine("Введите число от 10 до 99: ");
+int a = int.Parse(Console.ReadLine());
+Numbers (a);
