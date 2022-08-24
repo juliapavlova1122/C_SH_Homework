1071b100d3826553cb0ec24af8461faeccc9c208
Julia Pavlova <jul11112@yandex.ru>
2022-08-24 12:41:59 +0300

    10_2

---
+++ 10_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// 13. Выяснить, кратно ли число заданному, 
+// если нет, вывести остаток.
+
+void Multiplicity (int a, int b)
+{
+    if (a % b == 0)
+    Console.WriteLine ("It is ok");
+    else 
+    Console.WriteLine($"It is not ok, remainder is {a % b} ");
+}
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+Multiplicity (a, b);
+
